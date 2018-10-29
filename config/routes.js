dd192ac7a4cd80f3b087e7cb4d594ac48bd16426
David Benavidez <benavidez_david13@yahoo.com>
2018-10-29 13:05:45 -0700

    viewing note not working

---
+++ config/routes.js
@@ -0,0 +1,89 @@
+
+const db = require("../db/dbConfig");
+
+
+// exports
+module.exports = server => {
+    server.get("/notes/:id", viewNote);
+    server.post("/addNote", addNote);
+    server.get("/notes", getNotes);
+    server.put("/updateNote/:id", updateNote)
+    server.delete("/deleteNote/:id",deleteNote)
+};
+
+function addNote(req, res) {
+    const notes = req.body;
+    db("notes")
+        .insert(notes)
+        .then(ids => res.status(200).json(ids[0]))
+        .catch(err =>
+            res.status(500).json({ error: "Could not add note properly" })
+        );
+}
+
+function viewNote(req, res) {
+    const { id } = req.params;
+    db("notes")
+        .where({ id })
+        .then(notes => res.status(200).json({ notes }))
+        .catch(err => res.status(500).json(err));
+}
+
+function getNotes(req, res) {
+    db("notes")
+        .then(notes => res.status(200).json({ notes }))
+        .catch(err => res.status(500).json(err));
+}
+
+
+function updateNote(req, res) {
+    const { id } = req.params;
+    const changes = req.body;
+    db("notes")
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            if (!count || count < 1) {
+                res.status(404).json({ message: 'No note found to update' });
+            } else {
+                res.status(200).json(count);
+            }
+        })
+        .catch(err => res.status(500).json(err));
+};
+
+
+function updateNote(req, res) {
+    const { id } = req.params;
+    const changes = req.body;
+    db("notes")
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            if (!count || count < 1) {
+                res.status(404).json({ message: 'No note found to update' });
+            } else {
+                res.status(200).json(count);
+            }
+        })
+        .catch(err => res.status(500).json(err));
+};
+
+function deleteNote (req, res ) {
+    const { id } = req.params;
+      
+        db("notes")
+        .where({ id })
+        .del()
+          .then(count => {
+            if (!count || count < 1) {
+              res.status(404).json({ message: 'No note found to delete' });
+            } else {
+              res.status(200).json(count);
+            }
+          })
+          .catch(err => res.status(500).json(err));
+}
+
+
+
